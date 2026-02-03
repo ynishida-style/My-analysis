fabab9d5d1b57202268de5c2117470194ed23f1b
ynishida-style <yuto.nishida@cern.ch>
2026-02-03 16:14:34 +0900

    Add files via upload

---
+++ analysiscodes_forO2/jetanalysisnishida.sh
@@ -0,0 +1,29 @@
+#FileIn="$1"
+JSON="$1"
+#o2-analysis-timestamp -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-mccollision-converter -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-tracks-extra-v002-converter -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-track-to-collision-associator --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-je-emcal-correction-task -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-trackselection -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-propagationservice -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+#o2-analysis-event-selection -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+#o2-analysis-multiplicity-table -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-pid-tpc-service -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+#o2-analysis-pid-tof-merge -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-pid-tof-full -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-pid-tof-base -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-pid-tof-beta -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-ft0-corrected-table -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-multcenttable -b --configuration json://$JSON | \
+o2-analysis-event-selection-service -b --configuration json://$JSON | \
+o2-analysis-lf-mc-centrality -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+#o2-analysis-centrality-table -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-je-estimator-rho -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-je-jet-finder-data-charged -b --configuration json://$JSON | \
+#o2-analysis-je-jet-finder-data-full -b --configuration json://$JSON  --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+#o2-analysis-je-jet-finder-mcd-charged -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+#o2-analysis-je-jet-finder-mcp-charged -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+#o2-analysis-je-jet-matching-mc -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-je-jet-deriveddata-producer -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000 --min-failure-level error | \
+o2-analysis-je-jet-shape -b --configuration json://$JSON --aod-memory-rate-limit 2000000000 --shm-segment-size 16000000000
